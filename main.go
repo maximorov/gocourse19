6a20fc253892131e0a6b647c604e2cda9469dd93
maks <m.morozov@autodoc.eu>
2023-03-08 16:00:08 +0200

    little improve

--- main.go
+++ main.go
@@ -2,36 +2,10 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"github.com/redis/go-redis/v9"
 )
 
 var ctx = context.Background()
 
 func main() {
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "mycomplicatedpassword",
-		DB:       0, // use default DB
-	})
 
-	err := rdb.Set(ctx, "user:name", "Maks Morozov", 0).Err()
-	if err != nil {
-		panic(err)
-	}
-
-	val, err := rdb.Get(ctx, "user:name").Result()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("user:name", val)
-
-	val2, err := rdb.Get(ctx, "user:phone-number").Result()
-	if err == redis.Nil {
-		fmt.Println("user:phone-number does not exist")
-	} else if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("user:phone-number", val2)
-	}
 }
